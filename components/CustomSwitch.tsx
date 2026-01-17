c42c23221a73dac1984001af24cfc08849a077d2
Lucas Becker <beckerlucas19@gmail.com>
2026-01-17 15:48:36 -0300

    chore: add Material-UI and Emotion dependencies to package.json and add CustomSwitch component

---
+++ components/CustomSwitch.tsx
@@ -0,0 +1,32 @@
+import Switch from '@mui/material/Switch';
+import { styled } from '@mui/material/styles';
+
+const CustomSwitch = styled(Switch)(() => ({
+  width: 48,
+  height: 24,
+  padding: 0,
+  '& .MuiSwitch-switchBase': {
+    padding: 2,
+    '&.Mui-checked': {
+      transform: 'translateX(20px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        backgroundColor: '#FFB23D',
+        opacity: 1,
+      },
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    width: 12,
+    height: 12,
+    marginTop: 4,
+    marginLeft: 6,
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 28 / 2,
+    backgroundColor: '#53525D',
+    opacity: 1,
+  },
+}));
+
+export default CustomSwitch;
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,18 +2,50 @@
 
 import Image from "next/image";
 import Button from "./Button";
+import CustomSwitch from "./CustomSwitch";
+
+const ProjectsEmptyState = (
+    <div className="bg-content-background my-15 mx-card-x flex flex-1 justify-center items-center rounded">
+        <div className="flex flex-col justify-center items-center gap-6">
+            <p className="text-title font-semibold text-2xl leading-none text-center align-middle">Nenhum projeto</p>
+            <span className="text-description mb-2 font-normal text-base leading-22 text-center">Clique no botão abaixo para criar o primeiro e gerenciá-lo.</span>
+            <Button 
+                icon={<Image src="/images/plus-circle.svg" alt="Plus Circle" width={24} height={24} />} 
+                title="Novo projeto" 
+                action={() => undefined} 
+            />
+        </div>
+    </div>
+);
 
 export default function Projects () {
+    const isEmpty = false;
+
+    if (isEmpty) {
+        return ProjectsEmptyState;
+    }
+
     return (
-        <div className="bg-content-background my-15 mx-card-x flex flex-1 justify-center items-center rounded">
-            <div className="flex flex-col justify-center items-center gap-6">
-                <p className="text-title font-semibold text-2xl leading-none text-center align-middle">Nenhum projeto</p>
-                <span className="text-description mb-2 font-normal text-base leading-22 text-center">Clique no botão abaixo para criar o primeiro e gerenciá-lo.</span>
-                <Button 
-                    icon={<Image src="/images/plus-circle.svg" alt="Plus Circle" width={24} height={24} />} 
-                    title="Novo projeto" 
-                    action={() => undefined} 
-                />
+        <div className="my-15 mx-card-x">
+            <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                    <p>Projetos</p>
+                    <span>(9)</span>
+                </div>
+                <div className="flex items-center gap-8">
+                    <div className="flex items-center gap-2">
+                        <CustomSwitch />
+                        <span>Apenas Favoritos</span>
+                    </div>
+
+                    <div>Ordem alfabética</div>
+
+                    <Button 
+                        icon={<Image src="/images/plus-circle.svg" alt="Plus Circle" width={24} height={24} />} 
+                        title="Novo projeto" 
+                        action={() => undefined} 
+                    />
+                </div>
             </div>
         </div>
     );
